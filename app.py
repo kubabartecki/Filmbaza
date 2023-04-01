8d498658b841be087e423f5ed418b2977ecaff03
Bartłomiej Czajka <52079500+Resmakor@users.noreply.github.com>
2023-04-01 14:09:27 +0200

    Add first route
    Page with login form, default route

--- app.py
+++ app.py
@@ -3,5 +3,10 @@ from flask import Flask, render_template, redirect
 app = Flask(__name__)
 
 
+@app.route("/", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+
 if __name__ == "__main__":
     app.run()
