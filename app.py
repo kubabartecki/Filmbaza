041b1aea036edb57f8d06686166dd08065f2d5bf
Bartłomiej Czajka <52079500+Resmakor@users.noreply.github.com>
2023-04-01 17:00:53 +0200

    Error handling

--- app.py
+++ app.py
@@ -11,9 +11,17 @@ def default():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
+        if not request.form.get("name"):
+            return redirect("/register")
         name = request.form.get("name")
+        if not request.form.get("surname"):
+            return redirect("/register")
         surname = request.form.get("surname")
+        if not request.form.get("email"):
+            return redirect("/register")
         email = request.form.get("email")
+        if not request.form.get("password"):
+            return redirect("/register")
         password = request.form.get("password")
         print(f"{name} {surname} {email} {password}")
         return redirect("/")
@@ -24,7 +32,11 @@ def register():
 @app.route("/main_page", methods=["GET", "POST"])
 def main_page():
     if request.method == "POST":
+        if not request.form.get("email"):
+            return redirect("/")
         email = request.form.get("email")
+        if not request.form.get("password"):
+            return redirect("/")
         password = request.form.get("password")
         print(f"{email} {password}")
         return render_template("main_page.html", films=[], logged_user=[])
