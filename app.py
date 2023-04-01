0ad319977896aa7bce529e6b5ef22eaed56d8f70
Bartłomiej Czajka <52079500+Resmakor@users.noreply.github.com>
2023-04-01 14:26:38 +0200

    More routes and tiny front fixes
    - "/register" route renders register.html
    - "/main_page" route renders main_page.html
    - "/" default route renders login.html
    - Front href paths changed a bit to eliminate few bugs

--- app.py
+++ app.py
@@ -4,8 +4,18 @@ app = Flask(__name__)
 
 
 @app.route("/", methods=["GET", "POST"])
-def login():
+def default():
     return render_template("login.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
+
+
+@app.route("/main_page", methods=["GET", "POST"])
+def main_page():
+    return render_template("main_page.html", films=[], logged_user=[])
 
 
 if __name__ == "__main__":
