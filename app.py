12cb748dd79410dc1e3c7bdd5eb4d6d3629dfc8a
Bartłomiej Czajka <52079500+Resmakor@users.noreply.github.com>
2023-04-29 15:17:49 +0200

    Logic updates

--- app.py
+++ app.py
@@ -22,7 +22,6 @@ def clear_session():
     """Ensuring that the user is not logged in."""
     session.clear()
 
-
 @app.after_request
 def after_request(response):
     """Ensure responses aren't cached."""
@@ -52,72 +51,12 @@ def default():
     connection.close()
     return render_template("unloggedd.html", films=films, search_string=search_string)
 
-
 @app.route("/login", methods=["GET", "POST"])
 @login_not_required
 def login():
     checker = request.args.get("checker", default="True", type=str) == "True"
     message = request.args.get("message", default="", type=str)
     return render_template("login.html", checker=checker, message=message)
-
-@app.route("/main_page", methods=["GET", "POST"])
-@login_not_required
-def main_page():
-    """Route to user login validation, handles errors within default route."""
-    if request.method == "POST":
-        if not request.form.get("email"):
-            return redirect(url_for("login", checker="False", message="Email jest wymagany!"))
-        email = request.form.get("email")
-        if not request.form.get("password"):
-            return redirect(url_for("login", checker="False", message="Hasło jest wymagane!"))
-        password = request.form.get("password")
-        connection = psycopg2.connect(url)
-        cursor = connection.cursor()
-        cursor.execute(
-            "SELECT \"User\".id_user, \"User\".password FROM \"User\" WHERE \"User\".mail = %s", [email])
-        records = cursor.fetchall()
-        cursor.close()
-        connection.close()
-        if len(records) == 1:
-            user_id = records[0][0]
-            hash_from_db = records[0][1]
-        else:
-            return redirect(url_for("login", checker="False", message="Niepoprawny email!"))
-        if check_password_hash(hash_from_db, password):
-            session["user_id"] = user_id
-            return redirect("/home")
-        else:
-            return redirect(url_for("login", checker="False", message="Niepoprawny email lub hasło!"))
-    else:
-        return redirect("/login")
-
-@app.route("/home", methods=["GET", "POST"])
-@login_required
-def home():
-    """Main page of the website."""
-    connection = psycopg2.connect(url)
-    cursor = connection.cursor()
-    cursor.execute(
-        "SELECT * FROM \"User\" WHERE \"User\".id_user = %s", [session["user_id"]])
-    user_records = cursor.fetchall()
-    cursor.execute("SELECT COUNT(ID_REVIEW) FROM \"review\" INNER JOIN  \"User\" as u ON \"review\".User_ID_USER = u.ID_USER WHERE u.ID_USER = %s", [
-                   session["user_id"]])
-    user_reviews_count = cursor.fetchall()
-    # tutaj i w innych miejscach gdzie beda recenzje bedzie trzeba dodac czy przypadkiem ktos nie awansowal, a wtedy trzeba zmienic range
-    cursor.execute("SELECT film.ID_FILM, film.poster, film.title, film.director, film.year, film.description, STRING_AGG(country.name, ', ') countries, (SELECT AVG(review.stars) avg_grade FROM review WHERE review.film_id_film = film.id_film) FROM film_country JOIN film ON film.id_film = film_country.film_id_film JOIN country ON film_country.country_id_country = country.id_country GROUP BY film.id_film;")
-    films_records = cursor.fetchall()
-    search_string = request.args.get("search_string")
-    films = []
-    for row in films_records:
-        if search_string != None:
-            cursor.execute(
-                "SELECT c.name FROM category c JOIN film_category fc ON c.id_category = fc.category_id_category JOIN film f ON f.id_film = fc.film_id_film WHERE f.id_film = %s", [row[0]])
-            films.append(Film(row, cursor.fetchall()[0]))
-        else:
-            films.append(Film(row, [0]))
-    cursor.close()
-    connection.close()
-    return render_template("main_page.html", films=films, logged_user=logged_user(user_records, user_reviews_count), search_string=search_string)
 
 @app.route("/register", methods=["GET", "POST"])
 @login_not_required
@@ -168,6 +107,100 @@ def register():
         return redirect("/login")
     else:
         return render_template("register.html", checker=checker, message=message)
+
+@app.route("/main_page", methods=["GET", "POST"])
+@login_not_required
+def main_page():
+    """Route to user login validation, handles errors within default route."""
+    if request.method == "POST":
+        if not request.form.get("email"):
+            return redirect(url_for("login", checker="False", message="Email jest wymagany!"))
+        email = request.form.get("email")
+        if not request.form.get("password"):
+            return redirect(url_for("login", checker="False", message="Hasło jest wymagane!"))
+        password = request.form.get("password")
+        connection = psycopg2.connect(url)
+        cursor = connection.cursor()
+        cursor.execute(
+            "SELECT \"User\".id_user, \"User\".password FROM \"User\" WHERE \"User\".mail = %s", [email])
+        records = cursor.fetchall()
+        cursor.close()
+        connection.close()
+        if len(records) == 1:
+            user_id = records[0][0]
+            hash_from_db = records[0][1]
+        else:
+            return redirect(url_for("login", checker="False", message="Niepoprawny email!"))
+        if check_password_hash(hash_from_db, password):
+            session["user_id"] = user_id
+            return redirect("/home")
+        else:
+            return redirect(url_for("login", checker="False", message="Niepoprawny email lub hasło!"))
+    else:
+        return redirect("/login")
+
+@app.route("/home", methods=["GET", "POST"])
+@login_required
+def home():
+    """Main page of the website for logged user."""
+    connection = psycopg2.connect(url)
+    cursor = connection.cursor()
+    cursor.execute(
+        "SELECT * FROM \"User\" WHERE \"User\".id_user = %s", [session["user_id"]])
+    user_records = cursor.fetchall()
+    cursor.execute("SELECT COUNT(ID_REVIEW) FROM \"review\" INNER JOIN  \"User\" as u ON \"review\".User_ID_USER = u.ID_USER WHERE u.ID_USER = %s", [
+                   session["user_id"]])
+    user_reviews_count = cursor.fetchall()
+    # tutaj i w innych miejscach gdzie beda recenzje bedzie trzeba dodac czy przypadkiem ktos nie awansowal, a wtedy trzeba zmienic range
+    cursor.execute("SELECT film.ID_FILM, film.poster, film.title, film.director, film.year, film.description, STRING_AGG(country.name, ', ') countries, (SELECT AVG(review.stars) avg_grade FROM review WHERE review.film_id_film = film.id_film) FROM film_country JOIN film ON film.id_film = film_country.film_id_film JOIN country ON film_country.country_id_country = country.id_country GROUP BY film.id_film;")
+    films_records = cursor.fetchall()
+    search_string = request.args.get("search_string")
+    films = []
+    for row in films_records:
+        if search_string != None:
+            cursor.execute(
+                "SELECT c.name FROM category c JOIN film_category fc ON c.id_category = fc.category_id_category JOIN film f ON f.id_film = fc.film_id_film WHERE f.id_film = %s", [row[0]])
+            films.append(Film(row, cursor.fetchall()[0]))
+        else:
+            films.append(Film(row, [0]))
+    cursor.close()
+    connection.close()
+    return render_template("main_page.html", films=films, logged_user=logged_user(user_records, user_reviews_count), search_string=search_string)
+
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    """Route used to search for movies, redirects to home page with search_string appended as a query string parameter."""
+    if request.method == "POST":
+        search_string = request.form.get("search_string").strip().title()
+        if search_string != "":
+            if session.get("user_id") is None:
+                return redirect(f"/?search_string={search_string}")
+            else:
+                return redirect(f"/home?search_string={search_string}")
+    return redirect("/home")
+
+@app.route("/film_page", methods=["GET", "POST"])
+def film_page():
+    """Route used to display details about a specific movie."""
+    movie_id = request.form.get('film_butt')
+    connection = psycopg2.connect(url)
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM film WHERE id_film = %s", [movie_id])
+    film_id = cursor.fetchone()[0]
+    cursor.execute(
+        "SELECT film.poster, film.title, film.director, film.year, film.description, STRING_AGG(country.name, ', ') countries, (SELECT AVG(review.stars) avg_grade FROM review WHERE review.film_id_film = film.id_film) FROM film_country JOIN film ON film.id_film = film_country.film_id_film JOIN country ON film_country.country_id_country = country.id_country WHERE film.id_film = %s GROUP BY film.id_film", [film_id])
+    film_data = cursor.fetchone()
+    cursor.execute("SELECT u.username, r.description, r.stars FROM review r JOIN \"User\" u ON u.id_user = r.user_id_user WHERE r.film_id_film = %s;", [movie_id])
+    review_data = cursor.fetchall()
+    reviews = []
+    for row in review_data:
+        reviews.append(Review(row))
+    cursor.close()
+    connection.close()
+    return render_template('film_page.html', id=film_id, album=film_data[0], original_title=film_data[1], director=film_data[2], year=film_data[3], description=film_data[4], country=film_data[5], reviews=reviews)
+
+
+
 
 '''
 @app.route("/", methods=["GET", "POST"])
